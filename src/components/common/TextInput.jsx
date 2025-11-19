9c57eff98967a3988bd2f3f87f5057f0bcc80338
cmgjn1881 <cmgjn1881@naver.com>
2025-11-19 13:26:38 +0900

    feat: 포메이션에 심판 정보 추가 및 디자인 수정

--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -8,7 +8,7 @@ const TextInput = (props) => {
     // 💡 [수정] 테두리를 더 둥글게 하기 위해 rounded-lg를 rounded-xl로 변경합니다.
     <input
       {...props}
-      className="w-full px-4 py-1.5 border rounded-xl focus:ring-indigo-500 focus:border-indigo-500 text-xs bg-white placeholder:text-[#6B6B6B]"
+      className="w-full px-4 py-1.5 border rounded-xl focus:ring-indigo-500 focus:border-indigo-500 text-xs text-black bg-white placeholder:text-[#6B6B6B]"
     />
   );
 };
